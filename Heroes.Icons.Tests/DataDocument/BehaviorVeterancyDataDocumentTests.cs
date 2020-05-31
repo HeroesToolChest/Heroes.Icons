14e54e9c98ef5d8a8efaebdc05ce9a7e596741ee
Kevin Oliva <kevin.k.oliva@gmail.com>
2020-05-31 18:12:41 -0500

    Added behavior veterancy parsing

---
+++ Heroes.Icons.Tests/DataDocument/BehaviorVeterancyDataDocumentTests.cs
@@ -0,0 +1,221 @@
+﻿using Heroes.Icons.DataDocument;
+using Heroes.Models;
+using Heroes.Models.Veterancy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Heroes.Icons.Tests.DataDocument
+{
+    [TestClass]
+    public class BehaviorVeterancyDataDocumentTests : DataDocumentBase
+    {
+        private readonly string _dataFile = Path.Combine("JsonData", "behaviorveterancydata_76893_kokr.json");
+
+        private readonly BehaviorVeterancyDataDocument _behaviorVeterancyDataDocument;
+
+        public BehaviorVeterancyDataDocumentTests()
+        {
+            _behaviorVeterancyDataDocument = BehaviorVeterancyDataDocument.Parse(LoadJsonTestData(), Localization.ENUS);
+        }
+
+        [TestMethod]
+        [TestCategory("DataDocument")]
+        public void DataDocumentFileLocaleTest()
+        {
+            using BehaviorVeterancyDataDocument document = BehaviorVeterancyDataDocument.Parse(_dataFile, Localization.FRFR);
+
+            Assert.AreEqual(Localization.FRFR, document.Localization);
+            Assert.IsTrue(document.JsonDataDocument.RootElement.TryGetProperty("alteracpass-CoreScaling", out JsonElement jsonElement));
+            Assert.IsTrue(jsonElement!.GetProperty("combineXP").GetBoolean());
+        }
+
+        [TestMethod]
+        [TestCategory("DataDocument")]
+        public void DataDocumentFileTest()
+        {
+            using BehaviorVeterancyDataDocument document = BehaviorVeterancyDataDocument.Parse(_dataFile);
+
+            Assert.AreEqual(Localization.KOKR, document.Localization);
+            Assert.IsTrue(document.JsonDataDocument.RootElement.TryGetProperty("alteracpass-CoreScaling", out JsonElement _));
+        }
+
+        [TestMethod]
+        [TestCategory("DataDocument")]
+        public void DataDocumentROMLocaleTest()
+        {
+            using BehaviorVeterancyDataDocument document = BehaviorVeterancyDataDocument.Parse(GetBytesForROM("alteracpass-CoreScaling"), Localization.FRFR);
+
+            Assert.AreEqual(Localization.FRFR, document.Localization);
+            Assert.IsTrue(document.JsonDataDocument.RootElement.TryGetProperty("alteracpass-CoreScaling", out JsonElement _));
+        }
+
+        [TestMethod]
+        [TestCategory("DataDocument")]
+        public void DataDocumentStreamTest()
+        {
+            using FileStream stream = new FileStream(_dataFile, FileMode.Open);
+            using BehaviorVeterancyDataDocument document = BehaviorVeterancyDataDocument.Parse(stream, Localization.FRFR);
+
+            Assert.AreEqual(Localization.FRFR, document.Localization);
+            Assert.IsTrue(document.JsonDataDocument.RootElement.TryGetProperty("alteracpass-CoreScaling", out JsonElement _));
+        }
+
+        [TestMethod]
+        [TestCategory("DataDocument")]
+        public async Task DataDocumentStreamAsyncTest()
+        {
+            using FileStream stream = new FileStream(_dataFile, FileMode.Open);
+            using BehaviorVeterancyDataDocument document = await BehaviorVeterancyDataDocument.ParseAsync(stream, Localization.FRFR);
+
+            Assert.AreEqual(Localization.FRFR, document.Localization);
+            Assert.IsTrue(document.JsonDataDocument.RootElement.TryGetProperty("alteracpass-CoreScaling", out JsonElement _));
+        }
+
+        [DataTestMethod]
+        [DataRow("alteracpass-CoreScaling")]
+        [DataRow(null)]
+        [DataRow("asdf")]
+        public void GetSprayByIdTest(string id)
+        {
+            if (id is null)
+            {
+                Assert.ThrowsException<ArgumentNullException>(() =>
+                {
+                    _ = _behaviorVeterancyDataDocument.GetBehaviorVeterancyById(id!);
+                });
+
+                return;
+            }
+            else if (id == "asdf")
+            {
+                Assert.ThrowsException<KeyNotFoundException>(() =>
+                {
+                    _ = _behaviorVeterancyDataDocument.GetBehaviorVeterancyById(id);
+                });
+
+                return;
+            }
+
+            BasicAlteracpassCoreScalingAsserts(_behaviorVeterancyDataDocument.GetBehaviorVeterancyById(id));
+        }
+
+        [DataTestMethod]
+        [DataRow("alteracpass-CoreScaling")]
+        [DataRow(null)]
+        [DataRow("asdf")]
+        public void TryGetSprayByIdTest(string id)
+        {
+            if (id is null)
+            {
+                Assert.ThrowsException<ArgumentNullException>(() =>
+                {
+                    _ = _behaviorVeterancyDataDocument.TryGetBehaviorVeterancyById(id!, out _);
+                });
+
+                return;
+            }
+            else if (id == "asdf")
+            {
+                Assert.IsFalse(_behaviorVeterancyDataDocument.TryGetBehaviorVeterancyById(id, out _));
+
+                return;
+            }
+
+            Assert.IsTrue(_behaviorVeterancyDataDocument.TryGetBehaviorVeterancyById(id, out BehaviorVeterancy? _));
+            if (_behaviorVeterancyDataDocument.TryGetBehaviorVeterancyById(id, out BehaviorVeterancy? behaviorVeterancy))
+            {
+                BasicAlteracpassCoreScalingAsserts(behaviorVeterancy);
+            }
+        }
+
+        private static byte[] LoadJsonTestData()
+        {
+            using MemoryStream memoryStream = new MemoryStream();
+            using Utf8JsonWriter writer = new Utf8JsonWriter(memoryStream);
+            writer.WriteStartObject();
+
+            writer.WriteStartObject("alteracpass-CoreScaling");
+            writer.WriteBoolean("combineModifications", true);
+            writer.WriteBoolean("combineXP", true);
+            writer.WriteStartArray("veterancyLevels");
+
+            writer.WriteStartObject();
+            writer.WriteNumber("minVeterancyXP", 1);
+            writer.WriteEndObject();
+
+            writer.WriteStartObject();
+            writer.WriteNumber("minVeterancyXP", 1);
+            writer.WriteStartObject("modifications");
+            writer.WriteStartObject("vitalMax");
+            writer.WriteNumber("Life", 405);
+            writer.WriteNumber("Shields", 0);
+            writer.WriteEndObject(); // vital max
+            writer.WriteStartObject("vitalRegen");
+            writer.WriteNumber("Shields", 0);
+            writer.WriteEndObject(); // vital regen
+            writer.WriteEndObject(); // modifications
+            writer.WriteEndObject();
+
+            writer.WriteStartObject();
+            writer.WriteNumber("minVeterancyXP", 1);
+            writer.WriteStartObject("modifications");
+            writer.WriteNumber("killXPBonus", 1.0);
+            writer.WriteStartObject("damageDealtScaled");
+            writer.WriteNumber("Basic", 1.25);
+            writer.WriteEndObject();
+            writer.WriteStartObject("damageDealtFraction");
+            writer.WriteNumber("Ability", 0.05);
+            writer.WriteEndObject();
+            writer.WriteStartObject("vitalMaxFraction");
+            writer.WriteNumber("Life", 0.1);
+            writer.WriteEndObject();
+            writer.WriteStartObject("vitalRegenFraction");
+            writer.WriteNumber("Life", 1.10);
+            writer.WriteEndObject();
+            writer.WriteEndObject(); // modifications
+            writer.WriteEndObject();
+
+            writer.WriteEndArray();
+
+            writer.WriteEndObject();
+
+            writer.WriteEndObject();
+
+            writer.Flush();
+
+            return memoryStream.ToArray();
+        }
+
+        private static void BasicAlteracpassCoreScalingAsserts(BehaviorVeterancy behaviorVeterancy)
+        {
+            Assert.AreEqual("alteracpass-CoreScaling", behaviorVeterancy.Id);
+            Assert.IsTrue(behaviorVeterancy.CombineModifications);
+            Assert.IsTrue(behaviorVeterancy.CombineXP);
+
+            List<VeterancyLevel> list = behaviorVeterancy.VeterancyLevels.ToList();
+            Assert.AreEqual(1, list[0].MinimumVeterancyXP);
+            Assert.AreEqual("Life", list[1].VeterancyModification.VitalMaxCollection.ToList()[0].Type);
+            Assert.AreEqual(405, list[1].VeterancyModification.VitalMaxCollection.ToList()[0].Value);
+            Assert.AreEqual("Shields", list[1].VeterancyModification.VitalMaxCollection.ToList()[1].Type);
+            Assert.AreEqual(0, list[1].VeterancyModification.VitalMaxCollection.ToList()[1].Value);
+
+            Assert.AreEqual("Shields", list[1].VeterancyModification.VitalRegenCollection.ToList()[0].Type);
+            Assert.AreEqual(0, list[1].VeterancyModification.VitalRegenCollection.ToList()[0].Value);
+
+            Assert.AreEqual(1.0, list[2].VeterancyModification.KillXpBonus);
+            Assert.AreEqual("Basic", list[2].VeterancyModification.DamageDealtScaledCollection.ToList()[0].Type);
+            Assert.AreEqual(1.25, list[2].VeterancyModification.DamageDealtScaledCollection.ToList()[0].Value);
+            Assert.AreEqual("Ability", list[2].VeterancyModification.DamageDealtFractionCollection.ToList()[0].Type);
+            Assert.AreEqual(0.05, list[2].VeterancyModification.DamageDealtFractionCollection.ToList()[0].Value);
+            Assert.AreEqual("Life", list[2].VeterancyModification.VitalMaxFractionCollection.ToList()[0].Type);
+            Assert.AreEqual(0.1, list[2].VeterancyModification.VitalMaxFractionCollection.ToList()[0].Value);
+            Assert.AreEqual("Life", list[2].VeterancyModification.VitalRegenFractionCollection.ToList()[0].Type);
+            Assert.AreEqual(1.10, list[2].VeterancyModification.VitalRegenFractionCollection.ToList()[0].Value);
+        }
+    }
+}
--- Heroes.Icons/DataDocument/BehaviorVeterancyDataDocument.cs
+++ Heroes.Icons/DataDocument/BehaviorVeterancyDataDocument.cs
@@ -1,6 +1,264 @@
-﻿namespace Heroes.Icons.DataDocument
+﻿using Heroes.Models;
+using Heroes.Models.Veterancy;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Heroes.Icons.DataDocument
 {
-    class BehaviorVeterancyDataDocument
+    /// <summary>
+    /// Provides access to obtain <see cref="BehaviorVeterancy"/> data as well as updating localized strings.
+    /// </summary>
+    public class BehaviorVeterancyDataDocument : DataDocumentBase, IDataDocument
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BehaviorVeterancyDataDocument"/> class.
+        /// The <see cref="Localization"/> will be inferred from <paramref name="jsonDataFilePath"/>.
+        /// </summary>
+        /// <param name="jsonDataFilePath">The JSON data to parse.</param>
+        protected BehaviorVeterancyDataDocument(string jsonDataFilePath)
+            : base(jsonDataFilePath)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BehaviorVeterancyDataDocument"/> class.
+        /// </summary>
+        /// <param name="jsonDataFilePath">The JSON data to parse.</param>
+        /// <param name="localization">The <see cref="Localization"/> of the file.</param>
+        protected BehaviorVeterancyDataDocument(string jsonDataFilePath, Localization localization)
+            : base(jsonDataFilePath, localization)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BehaviorVeterancyDataDocument"/> class.
+        /// </summary>
+        /// <param name="jsonData">The JSON data to parse.</param>
+        /// <param name="localization">The <see cref="Localization"/> of the file.</param>
+        protected BehaviorVeterancyDataDocument(ReadOnlyMemory<byte> jsonData, Localization localization)
+            : base(jsonData, localization)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BehaviorVeterancyDataDocument"/> class.
+        /// </summary>
+        /// <param name="utf8Json">The JSON data to parse.</param>
+        /// <param name="localization">The <see cref="Localization"/> of the file.</param>
+        /// <param name="isAsync">Value indicating whether to parse the <paramref name="utf8Json"/> as <see langword="async"/>.</param>
+        protected BehaviorVeterancyDataDocument(Stream utf8Json, Localization localization, bool isAsync = false)
+            : base(utf8Json, localization, isAsync)
+        {
+        }
+
+        /// <summary>
+        /// Parses a json file as UTF-8-encoded text to allow for <see cref="BehaviorVeterancy"/> data reading.
+        /// The <see cref="Localization"/> will be inferred from <paramref name="jsonDataFilePath"/>.
+        /// </summary>
+        /// <param name="jsonDataFilePath">The JSON data to parse.</param>
+        /// <returns>A <see cref="BehaviorVeterancyDataDocument"/> representation of the JSON value.</returns>
+        public static BehaviorVeterancyDataDocument Parse(string jsonDataFilePath)
+        {
+            return new BehaviorVeterancyDataDocument(jsonDataFilePath);
+        }
+
+        /// <summary>
+        /// Parses a json file as UTF-8-encoded text to allow for <see cref="BehaviorVeterancy"/> data reading.
+        /// </summary>
+        /// <param name="jsonDataFilePath">The JSON data to parse.</param>
+        /// <param name="localization">The <see cref="Localization"/> of the file.</param>
+        /// <returns>A <see cref="BehaviorVeterancyDataDocument"/> representation of the JSON value.</returns>
+        public static BehaviorVeterancyDataDocument Parse(string jsonDataFilePath, Localization localization)
+        {
+            return new BehaviorVeterancyDataDocument(jsonDataFilePath, localization);
+        }
+
+        /// <summary>
+        /// Parses a json file as UTF-8-encoded text to allow for <see cref="BehaviorVeterancy"/> data reading.
+        /// </summary>
+        /// <param name="jsonData">The JSON data to parse.</param>
+        /// <param name="localization">The <see cref="Localization"/> of the file.</param>
+        /// <returns>A <see cref="BehaviorVeterancyDataDocument"/> representation of the JSON value.</returns>
+        public static BehaviorVeterancyDataDocument Parse(ReadOnlyMemory<byte> jsonData, Localization localization)
+        {
+            return new BehaviorVeterancyDataDocument(jsonData, localization);
+        }
+
+        /// <summary>
+        /// Parses a json file as UTF-8-encoded text to allow for <see cref="BehaviorVeterancy"/> data reading.
+        /// </summary>
+        /// <param name="utf8Json">The JSON data to parse.</param>
+        /// <param name="localization">The <see cref="Localization"/> of the file.</param>
+        /// <returns>A <see cref="BehaviorVeterancyDataDocument"/> representation of the JSON value.</returns>
+        public static BehaviorVeterancyDataDocument Parse(Stream utf8Json, Localization localization)
+        {
+            return new BehaviorVeterancyDataDocument(utf8Json, localization);
+        }
+
+        /// <summary>
+        /// Parses a json file as UTF-8-encoded text to allow for <see cref="BehaviorVeterancy"/> data reading.
+        /// </summary>
+        /// <param name="utf8Json">The JSON data to parse.</param>
+        /// <param name="localization">The <see cref="Localization"/> of the file.</param>
+        /// <returns>A <see cref="BehaviorVeterancyDataDocument"/> representation of the JSON value.</returns>
+        public static Task<BehaviorVeterancyDataDocument> ParseAsync(Stream utf8Json, Localization localization)
+        {
+            return new BehaviorVeterancyDataDocument(utf8Json, localization, true).InitializeParseDataStreamAsync<BehaviorVeterancyDataDocument>();
+        }
+
+        /// <summary>
+        /// Gets a <see cref="BehaviorVeterancy"/> from the behavior veterancy <paramref name="id"/> property value.
+        /// </summary>
+        /// <param name="id">A behavior veterancy id property value.</param>
+        /// <returns>A <see cref="BehaviorVeterancy"/> object.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="id"/> is <see langword="null"/>.</exception>
+        /// <exception cref="KeyNotFoundException">The <paramref name="id"/> property value was not found.</exception>
+        public BehaviorVeterancy GetBehaviorVeterancyById(string id)
+        {
+            if (id is null)
+                throw new ArgumentNullException(nameof(id));
+
+            if (TryGetBehaviorVeterancyById(id, out BehaviorVeterancy? value))
+                return value;
+            else
+                throw new KeyNotFoundException();
+        }
+
+        /// <summary>
+        /// Looks for a behavior veterancy with the <paramref name="id"/> property value, returning a value that indicates whether such value exists.
+        /// </summary>
+        /// <param name="id">A behavior veterancy id property value.</param>
+        /// <param name="value">When this method returns, contains the <see cref="BehaviorVeterancy"/> associated with the <paramref name="id"/> property value.</param>
+        /// <returns><see langword="true"/> if the value was found; otherwise <see langword="false"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="id"/> is <see langword="null"/>.</exception>
+        public bool TryGetBehaviorVeterancyById(string id, [NotNullWhen(true)] out BehaviorVeterancy? value)
+        {
+            if (id is null)
+                throw new ArgumentNullException(nameof(id));
+
+            value = null;
+
+            if (JsonDataDocument.RootElement.TryGetProperty(id, out JsonElement element))
+            {
+                value = GetBehaviorVeterancyData(id, element);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private static BehaviorVeterancy GetBehaviorVeterancyData(string behaviorVeterancyId, JsonElement behaviorVeterancyElement)
+        {
+            BehaviorVeterancy behaviorVeterancy = new BehaviorVeterancy()
+            {
+                Id = behaviorVeterancyId,
+            };
+
+            if (behaviorVeterancyElement.TryGetProperty("combineModifications", out JsonElement combineModifications))
+                behaviorVeterancy.CombineModifications = combineModifications.GetBoolean();
+
+            if (behaviorVeterancyElement.TryGetProperty("combineXP", out JsonElement combineXP))
+                behaviorVeterancy.CombineXP = combineXP.GetBoolean();
+
+            if (behaviorVeterancyElement.TryGetProperty("veterancyLevels", out JsonElement veterancyLevelsElement))
+            {
+                foreach (JsonElement veterancyLevelElement in veterancyLevelsElement.EnumerateArray())
+                {
+                    VeterancyLevel veterancyLevel = new VeterancyLevel();
+
+                    if (veterancyLevelElement.TryGetProperty("minVeterancyXP", out JsonElement minVeterancyXP))
+                    {
+                        veterancyLevel.MinimumVeterancyXP = minVeterancyXP.GetInt32();
+                    }
+
+                    if (veterancyLevelElement.TryGetProperty("modifications", out JsonElement modificationsElement))
+                    {
+                        if (modificationsElement.TryGetProperty("killXPBonus", out JsonElement killXPBonus))
+                            veterancyLevel.VeterancyModification.KillXpBonus = killXPBonus.GetDouble();
+
+                        if (modificationsElement.TryGetProperty("damageDealtScaled", out JsonElement damageDealtScaledElement))
+                        {
+                            foreach (JsonProperty property in damageDealtScaledElement.EnumerateObject())
+                            {
+                                veterancyLevel.VeterancyModification.DamageDealtScaledCollection.Add(new VeterancyDamageDealtScaled()
+                                {
+                                    Type = property.Name,
+                                    Value = property.Value.GetDouble(),
+                                });
+                            }
+                        }
+
+                        if (modificationsElement.TryGetProperty("damageDealtFraction", out JsonElement damageDealtFractionElement))
+                        {
+                            foreach (JsonProperty property in damageDealtFractionElement.EnumerateObject())
+                            {
+                                veterancyLevel.VeterancyModification.DamageDealtFractionCollection.Add(new VeterancyDamageDealtFraction()
+                                {
+                                    Type = property.Name,
+                                    Value = property.Value.GetDouble(),
+                                });
+                            }
+                        }
+
+                        if (modificationsElement.TryGetProperty("vitalMax", out JsonElement vitalMaxElement))
+                        {
+                            foreach (JsonProperty property in vitalMaxElement.EnumerateObject())
+                            {
+                                veterancyLevel.VeterancyModification.VitalMaxCollection.Add(new VeterancyVitalMax()
+                                {
+                                    Type = property.Name,
+                                    Value = property.Value.GetDouble(),
+                                });
+                            }
+                        }
+
+                        if (modificationsElement.TryGetProperty("vitalMaxFraction", out JsonElement vitalMaxFractionElement))
+                        {
+                            foreach (JsonProperty property in vitalMaxFractionElement.EnumerateObject())
+                            {
+                                veterancyLevel.VeterancyModification.VitalMaxFractionCollection.Add(new VeterancyVitalMaxFraction()
+                                {
+                                    Type = property.Name,
+                                    Value = property.Value.GetDouble(),
+                                });
+                            }
+                        }
+
+                        if (modificationsElement.TryGetProperty("vitalRegen", out JsonElement vitalRegenElement))
+                        {
+                            foreach (JsonProperty property in vitalRegenElement.EnumerateObject())
+                            {
+                                veterancyLevel.VeterancyModification.VitalRegenCollection.Add(new VeterancyVitalRegen()
+                                {
+                                    Type = property.Name,
+                                    Value = property.Value.GetDouble(),
+                                });
+                            }
+                        }
+
+                        if (modificationsElement.TryGetProperty("vitalRegenFraction", out JsonElement vitalRegenFractionElement))
+                        {
+                            foreach (JsonProperty property in vitalRegenFractionElement.EnumerateObject())
+                            {
+                                veterancyLevel.VeterancyModification.VitalRegenFractionCollection.Add(new VeterancyVitalRegenFraction()
+                                {
+                                    Type = property.Name,
+                                    Value = property.Value.GetDouble(),
+                                });
+                            }
+                        }
+                    }
+
+                    behaviorVeterancy.VeterancyLevels.Add(veterancyLevel);
+                }
+            }
+
+            return behaviorVeterancy;
+        }
     }
 }
