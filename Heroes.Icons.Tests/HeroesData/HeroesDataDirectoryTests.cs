ad09c04c1cfddd416b2dc0a51c59ac25e7b125ab
Kevin Oliva <kevin.k.oliva@gmail.com>
2020-06-11 21:08:20 -0500

    Added HeroesDataDirectory

---
+++ Heroes.Icons.Tests/HeroesData/HeroesDataDirectoryTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Heroes.Icons.HeroesData.Tests
+{
+    [TestClass]
+    public class HeroesDataDirectoryTests
+    {
+        private readonly string _testFolder = "heroesdataTestFolders";
+
+        [TestMethod]
+        public void GetVersionsTest()
+        {
+            List<HeroesDataVersion> list = HeroesDataDirectory.GetVersions(_testFolder).ToList();
+
+            HeroesDataVersion first = list[0];
+
+            Assert.AreEqual(3, list.Count);
+            Assert.AreEqual(76003, first.Build);
+            Assert.AreEqual(2, first.Major);
+            Assert.AreEqual(47, first.Minor);
+            Assert.AreEqual(2, first.Revision);
+            Assert.IsFalse(first.IsPtr);
+
+            HeroesDataVersion third = list[2];
+
+            Assert.AreEqual(76268, third.Build);
+            Assert.AreEqual(2, third.Major);
+            Assert.AreEqual(48, third.Minor);
+            Assert.AreEqual(0, third.Revision);
+            Assert.IsTrue(third.IsPtr);
+        }
+
+        [TestMethod]
+        public void GetVersionsExceptionsTest()
+        {
+            Assert.ThrowsException<ArgumentException>(() => HeroesDataDirectory.GetVersions(null!));
+            Assert.ThrowsException<ArgumentException>(() => HeroesDataDirectory.GetVersions(string.Empty));
+            Assert.ThrowsException<ArgumentException>(() => HeroesDataDirectory.GetVersions(" "));
+        }
+
+        [TestMethod]
+        public void VersionExistsTest()
+        {
+            Assert.IsTrue(HeroesDataDirectory.VersionExists(_testFolder, new HeroesDataVersion(2, 47, 3, 76124)));
+            Assert.IsTrue(HeroesDataDirectory.VersionExists(_testFolder, new HeroesDataVersion(2, 48, 0, 76268, true)));
+            Assert.IsFalse(HeroesDataDirectory.VersionExists(_testFolder, new HeroesDataVersion(77, 23, 1, 76268, true)));
+        }
+
+        [TestMethod]
+        public void VersionExistsExceptionsTest()
+        {
+            Assert.ThrowsException<ArgumentException>(() => HeroesDataDirectory.VersionExists(null!, new HeroesDataVersion(1, 1, 1, 1)));
+            Assert.ThrowsException<ArgumentException>(() => HeroesDataDirectory.VersionExists(string.Empty, new HeroesDataVersion(1, 1, 1, 1)));
+            Assert.ThrowsException<ArgumentException>(() => HeroesDataDirectory.VersionExists(" ", new HeroesDataVersion(1, 1, 1, 1)));
+            Assert.ThrowsException<ArgumentNullException>(() => HeroesDataDirectory.VersionExists("path", null!));
+        }
+    }
+}
---
+++ Heroes.Icons/HeroesData/HeroesDataDirectory.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Heroes.Icons.HeroesData
+{
+    /// <summary>
+    /// Contains the information for the heroes-data directory.
+    /// </summary>
+    public static class HeroesDataDirectory
+    {
+        /// <summary>
+        /// Gets a collection of <see cref="HeroesDataVersion"/>s in the heroes-data directory.
+        /// </summary>
+        /// <param name="path">The directory that contains the heroes-data version directories.</param>
+        /// <returns>A collection of <see cref="HeroesDataVersion"/>s.</returns>
+        /// <exception cref="ArgumentException"><paramref name="path"/> is <see langword="null"/> or whitespace.</exception>
+        public static IEnumerable<HeroesDataVersion> GetVersions(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                throw new ArgumentException("Cannot be null or whitespace", nameof(path));
+            }
+
+            List<HeroesDataVersion> heroesDataVersions = new List<HeroesDataVersion>();
+
+            IEnumerable<string> pathDirectories = Directory.EnumerateDirectories(path, "*.*.*.*");
+
+            foreach (string directoryPath in pathDirectories)
+            {
+                string name = new DirectoryInfo(directoryPath).Name;
+
+                if (!string.IsNullOrWhiteSpace(name))
+                {
+                    if (HeroesDataVersion.TryParse(name, out HeroesDataVersion? result))
+                    {
+                        heroesDataVersions.Add(result);
+                    }
+                }
+            }
+
+            return heroesDataVersions;
+        }
+
+        /// <summary>
+        /// Checks if the version exists as a directory in the <paramref name="path"/>.
+        /// </summary>
+        /// <param name="path">The directory to check.</param>
+        /// <param name="heroesVersion">The version to find.</param>
+        /// <returns><see langword="true"/> if the version was found; otherwise <see langword="false"/>.</returns>
+        public static bool VersionExists(string path, HeroesDataVersion heroesVersion)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+                throw new ArgumentException("Cannot be null or whitespace", nameof(path));
+
+            if (heroesVersion is null)
+                throw new ArgumentNullException(nameof(heroesVersion));
+
+            return Directory.Exists(Path.Combine(path, heroesVersion.ToString()!));
+        }
+    }
+}
