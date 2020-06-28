b037af031b1e19791fb2f64afaa202c75bafc09c
Kevin Oliva <kevin.k.oliva@gmail.com>
2020-06-27 23:05:09 -0500

    Updated more methods to HeroesDataDirectory

--- Heroes.Icons.Tests/HeroesData/HeroesDataDirectoryTests.cs
+++ Heroes.Icons.Tests/HeroesData/HeroesDataDirectoryTests.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Heroes.Icons.DataDocument;
+using Heroes.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace Heroes.Icons.HeroesData.Tests
@@ -9,41 +12,51 @@ namespace Heroes.Icons.HeroesData.Tests
     public class HeroesDataDirectoryTests
     {
         private readonly string _testFolder = "heroesdataTestFolders";
+        private readonly HeroesDataDirectory _heroesDataDirectory;
 
-        [TestMethod]
-        public void GetVersionsTest()
+        public HeroesDataDirectoryTests()
         {
-            List<HeroesDataVersion> list = HeroesDataDirectory.GetVersions(_testFolder).ToList();
-
-            HeroesDataVersion first = list[0];
-
-            Assert.AreEqual(3, list.Count);
-
-            Assert.AreEqual(76268, first.Build);
-            Assert.AreEqual(2, first.Major);
-            Assert.AreEqual(48, first.Minor);
-            Assert.AreEqual(0, first.Revision);
-            Assert.IsTrue(first.IsPtr);
-
-            HeroesDataVersion third = list[2];
-
-            Assert.AreEqual(76003, third.Build);
-            Assert.AreEqual(2, third.Major);
-            Assert.AreEqual(47, third.Minor);
-            Assert.AreEqual(2, third.Revision);
-            Assert.IsFalse(third.IsPtr);
+            _heroesDataDirectory = new HeroesDataDirectory("heroesdataTestFolders");
         }
 
         [TestMethod]
-        public void GetVersionsExceptionsTest()
+        public void HeroesDataDirectoryContructorThrowExceptionsTest()
         {
-            Assert.ThrowsException<ArgumentException>(() => HeroesDataDirectory.GetVersions(null!));
-            Assert.ThrowsException<ArgumentException>(() => HeroesDataDirectory.GetVersions(string.Empty));
-            Assert.ThrowsException<ArgumentException>(() => HeroesDataDirectory.GetVersions(" "));
+            Assert.ThrowsException<ArgumentException>(() => new HeroesDataDirectory(null!));
+            Assert.ThrowsException<ArgumentException>(() => new HeroesDataDirectory(string.Empty));
+            Assert.ThrowsException<DirectoryNotFoundException>(() => new HeroesDataDirectory("someDirectory"));
         }
 
         [TestMethod]
-        public void VersionExistsTest()
+        public void NoVersionsLoadedTest()
+        {
+            HeroesDataDirectory heroesDataDirectory = new HeroesDataDirectory(Path.Join("heroesdataTestFolders", "someOther"));
+            Assert.IsNull(heroesDataDirectory.NewestVersion);
+            Assert.IsNull(heroesDataDirectory.OldestVersion);
+        }
+
+        [TestMethod]
+        public void GetVersionsStaticMethodTest()
+        {
+            List<HeroesDataVersion> list = HeroesDataDirectory.GetVersions(_testFolder).OrderByDescending(x => x).ToList();
+            Assert.AreEqual(0, list.Where(x => x.IsPtr).Count());
+            Assert.AreEqual(6, list.Count);
+
+            List<HeroesDataVersion> listWithPtr = HeroesDataDirectory.GetVersions(_testFolder, true).OrderByDescending(x => x).ToList();
+            Assert.IsTrue(listWithPtr.Where(x => x.IsPtr).Any());
+            Assert.AreEqual(8, listWithPtr.Count);
+        }
+
+        [TestMethod]
+        public void GetVersionsStaticMethodExceptionsTest()
+        {
+            Assert.ThrowsException<ArgumentException>(() => HeroesDataDirectory.GetVersions(null!).ToList());
+            Assert.ThrowsException<ArgumentException>(() => HeroesDataDirectory.GetVersions(string.Empty).ToList());
+            Assert.ThrowsException<ArgumentException>(() => HeroesDataDirectory.GetVersions(" ").ToList());
+        }
+
+        [TestMethod]
+        public void VersionExistsStaticMethodTest()
         {
             Assert.IsTrue(HeroesDataDirectory.VersionExists(_testFolder, new HeroesDataVersion(2, 47, 3, 76124)));
             Assert.IsTrue(HeroesDataDirectory.VersionExists(_testFolder, new HeroesDataVersion(2, 48, 0, 76268, true)));
@@ -51,12 +64,72 @@ namespace Heroes.Icons.HeroesData.Tests
         }
 
         [TestMethod]
-        public void VersionExistsExceptionsTest()
+        public void VersionExistsStaticMethodExceptionsTest()
         {
             Assert.ThrowsException<ArgumentException>(() => HeroesDataDirectory.VersionExists(null!, new HeroesDataVersion(1, 1, 1, 1)));
             Assert.ThrowsException<ArgumentException>(() => HeroesDataDirectory.VersionExists(string.Empty, new HeroesDataVersion(1, 1, 1, 1)));
             Assert.ThrowsException<ArgumentException>(() => HeroesDataDirectory.VersionExists(" ", new HeroesDataVersion(1, 1, 1, 1)));
             Assert.ThrowsException<ArgumentNullException>(() => HeroesDataDirectory.VersionExists("path", null!));
+        }
+
+        [TestMethod]
+        public void VersionsTest()
+        {
+            List<HeroesDataVersion> list = _heroesDataDirectory.Versions.ToList();
+            Assert.AreEqual(8, list.Count);
+        }
+
+        [TestMethod]
+        public void VersionExistsTest()
+        {
+            Assert.IsFalse(_heroesDataDirectory.VersionExists(new HeroesDataVersion(-1, 234, 234, 34234234)));
+            Assert.IsTrue(_heroesDataDirectory.VersionExists(new HeroesDataVersion(2, 47, 3, 76124)));
+        }
+
+        [TestMethod]
+        public void BuildExistsTest()
+        {
+            Assert.IsFalse(_heroesDataDirectory.BuildExists(34234234));
+            Assert.IsTrue(_heroesDataDirectory.BuildExists(76124));
+        }
+
+        [DataTestMethod]
+        [DataRow(2, 47, 3, 76124, false, true, Localization.KOKR)]
+        [DataRow(2, 47, 3, 76124, false, false, Localization.KOKR)]
+        [DataRow(2, 40, 3, 70000, false, false, Localization.KOKR)]
+        [DataRow(2, 48, 3, 77205, false, false, Localization.KOKR)]
+        [DataRow(2, 48, 3, 77205, false, true, Localization.KOKR)]
+        [DataRow(2, 48, 4, 77406, false, true, Localization.KOKR)]
+        [DataRow(2, 48, 4, 77407, false, true, Localization.KOKR)]
+        [DataRow(8, 50, 3, 9999999, false, false, Localization.KOKR)]
+        public void GetHeroDataTest(int major, int minor, int revision, int build, bool isPtr, bool gamestrings, Localization localization)
+        {
+            HeroDataDocument heroDataDocument = _heroesDataDirectory.HeroData(new HeroesDataVersion(major, minor, revision, build, isPtr), gamestrings, localization);
+            Assert.IsTrue(heroDataDocument.TryGetHeroById("Abathur", out Hero? hero, false, false, false, false));
+
+            if (gamestrings)
+                Assert.IsTrue(!string.IsNullOrEmpty(hero!.Description?.RawDescription));
+            else
+                Assert.IsFalse(!string.IsNullOrEmpty(hero!.Description?.RawDescription));
+        }
+
+        [DataTestMethod]
+        [DataRow(2, 48, 3, 77205, false, true, Localization.KOKR)]
+        public void GetHeroDataLoadDuplicateDataTest(int major, int minor, int revision, int build, bool isPtr, bool gamestrings, Localization localization)
+        {
+            HeroDataDocument heroDataDocument = _heroesDataDirectory.HeroData(new HeroesDataVersion(major, minor, revision, build, isPtr), gamestrings, localization);
+            Assert.IsTrue(heroDataDocument.TryGetHeroById("Abathur", out Hero? hero, false, false, false, false));
+
+            if (gamestrings)
+                Assert.IsTrue(!string.IsNullOrEmpty("this is some text."));
+            else
+                Assert.IsFalse(!string.IsNullOrEmpty(hero!.Description?.RawDescription));
+        }
+
+        [TestMethod]
+        public void HeroDataExceptionsTest()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => _heroesDataDirectory.HeroData(null!, true, Localization.KOKR));
         }
     }
 }
--- Heroes.Icons/HeroesData/HeroesDataDirectory.cs
+++ Heroes.Icons/HeroesData/HeroesDataDirectory.cs
@@ -1,46 +1,103 @@
-﻿using System;
+﻿using Heroes.Icons.DataDocument;
+using Heroes.Models;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Text.Json;
 
 namespace Heroes.Icons.HeroesData
 {
     /// <summary>
     /// Contains the information for the heroes-data directory.
     /// </summary>
-    public static class HeroesDataDirectory
+    [SuppressMessage("Globalization", "CA1308:Normalize strings to uppercase", Justification = "need lower")]
+    [SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "will be disposed by caller.")]
+    public class HeroesDataDirectory
     {
+        private const string _hdpJsonFile = ".hdp.json";
+        private const string _announcerFileTemplateName = "announcerdata_{0}_localized.json";
+        private const string _heroFileTemplateName = "herodata_{0}_localized.json";
+
+        private const string _gameStringTemplateName = "gamestrings_{0}_{1}.json";
+
+        private readonly string _heroesDataDirectoryPath;
+        private readonly SortedSet<HeroesDataVersion> _versions;
+
         /// <summary>
-        /// Gets a collection of <see cref="HeroesDataVersion"/>s in the heroes-data directory ordered by newest to oldest (descending).
+        /// Initializes a new instance of the <see cref="HeroesDataDirectory"/> class. Loads all the version directories.
         /// </summary>
-        /// <param name="path">The directory that contains the heroes-data version directories.</param>
+        /// <param name="heroesDataPath">The path of the heroes-data directory.</param>
+        public HeroesDataDirectory(string heroesDataPath = "heroesdata")
+        {
+            if (string.IsNullOrWhiteSpace(heroesDataPath))
+                throw new ArgumentException("Cannot be null or whitespace", nameof(heroesDataPath));
+
+            if (!Directory.Exists(heroesDataPath))
+                throw new DirectoryNotFoundException("Path does not exist as a directory");
+
+            _heroesDataDirectoryPath = heroesDataPath;
+
+            _versions = new SortedSet<HeroesDataVersion>(GetVersions(_heroesDataDirectoryPath, true));
+        }
+
+        /// <summary>
+        /// Gets the newest (latest) version.
+        /// </summary>
+        public HeroesDataVersion? NewestVersion => _versions.Max;
+
+        /// <summary>
+        /// Gets the oldest (earliest) version.
+        /// </summary>
+        public HeroesDataVersion? OldestVersion => _versions.Min;
+
+        /// <summary>
+        /// Gets a collection of versions.
+        /// </summary>
+        public IEnumerable<HeroesDataVersion> Versions => _versions;
+
+        /// <summary>
+        /// Gets or sets the name of the data directory.
+        /// </summary>
+        public string DataDirectoryName { get; set; } = "data";
+
+        /// <summary>
+        /// Gets or sets the name of the gamestring directory.
+        /// </summary>
+        public string GameStringsDirectoryName { get; set; } = "gamestrings";
+
+        /// <summary>
+        /// Gets a collection of <see cref="HeroesDataVersion"/>s from the <paramref name="path"/>.
+        /// </summary>
+        /// <param name="path">The path that contains the version directories.</param>
+        /// <param name="includePtr"><see langword="true"/> to include the ptr versions.</param>
         /// <returns>A collection of <see cref="HeroesDataVersion"/>s.</returns>
         /// <exception cref="ArgumentException"><paramref name="path"/> is <see langword="null"/> or whitespace.</exception>
-        public static IEnumerable<HeroesDataVersion> GetVersions(string path)
+        public static IEnumerable<HeroesDataVersion> GetVersions(string path, bool includePtr = false)
         {
             if (string.IsNullOrWhiteSpace(path))
-            {
                 throw new ArgumentException("Cannot be null or whitespace", nameof(path));
-            }
-
-            List<HeroesDataVersion> heroesDataVersions = new List<HeroesDataVersion>();
 
             IEnumerable<string> pathDirectories = Directory.EnumerateDirectories(path, "*.*.*.*");
 
             foreach (string directoryPath in pathDirectories)
             {
-                string name = new DirectoryInfo(directoryPath).Name;
+                ReadOnlySpan<char> pathSpan = Path.TrimEndingDirectorySeparator(directoryPath.AsSpan());
+                ReadOnlySpan<char> name = pathSpan.Slice(pathSpan.LastIndexOf(Path.DirectorySeparatorChar) + 1);
 
-                if (!string.IsNullOrWhiteSpace(name))
+                if (!name.IsEmpty)
                 {
                     if (HeroesDataVersion.TryParse(name, out HeroesDataVersion? result))
                     {
-                        heroesDataVersions.Add(result);
+                        if (result.IsPtr && !includePtr)
+                            continue;
+
+                        yield return result;
                     }
                 }
             }
-
-            return heroesDataVersions.OrderByDescending(x => x);
         }
 
         /// <summary>
@@ -48,7 +105,9 @@ namespace Heroes.Icons.HeroesData
         /// </summary>
         /// <param name="path">The directory to check.</param>
         /// <param name="heroesVersion">The version to find.</param>
-        /// <returns><see langword="true"/> if the version was found; otherwise <see langword="false"/>.</returns>
+        /// <returns><see langword="true"/> if the <paramref name="heroesVersion"/> directory was found at the <paramref name="path"/>; otherwise <see langword="false"/>.</returns>
+        /// <exception cref="ArgumentException"><paramref name="path"/> is <see langword="null"/> or whitespace.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="heroesVersion"/> is <see langword="null"/>.</exception>
         public static bool VersionExists(string path, HeroesDataVersion heroesVersion)
         {
             if (string.IsNullOrWhiteSpace(path))
@@ -58,6 +117,110 @@ namespace Heroes.Icons.HeroesData
                 throw new ArgumentNullException(nameof(heroesVersion));
 
             return Directory.Exists(Path.Combine(path, heroesVersion.ToString()!));
+        }
+
+        /// <summary>
+        /// Checks if the version exists.
+        /// </summary>
+        /// <param name="heroesVersion">The version to find.</param>
+        /// <returns><see langword="true"/> if the <paramref name="heroesVersion"/> exists, otherwise <see langword="false"/>.</returns>
+        public bool VersionExists(HeroesDataVersion heroesVersion) => _versions.Contains(heroesVersion);
+
+        /// <summary>
+        /// Checks if the build exists.
+        /// </summary>
+        /// <param name="build">The build to find.</param>
+        /// <returns><see langword="true"/> if the <paramref name="build"/> exists, otherwise <see langword="false"/>.</returns>
+        public bool BuildExists(int build) => _versions.Any(x => x.Build == build);
+
+        /// <summary>
+        /// Loads the <see cref="HeroDataDocument"/> from the <paramref name="version"/> directory.
+        /// </summary>
+        /// <param name="version">The version directory to load the data from.</param>
+        /// <param name="includeGameStrings">If <see langword="true"/>, loads the gamestrings from the gamestrings directory.</param>
+        /// <param name="localization">The <see cref="Localization"/> of the gamestrings.</param>
+        /// <returns>The <see cref="HeroDataDocument"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="version"/> is <see langword="null"/>.</exception>
+        public HeroDataDocument HeroData(HeroesDataVersion version, bool includeGameStrings = true, Localization localization = Localization.ENUS)
+        {
+            if (version is null)
+                throw new ArgumentNullException(nameof(version));
+
+            VersionCheck(ref version);
+
+            if (version is null)
+                throw new ArgumentNullException(nameof(version));
+
+            string versionString = version.ToString();
+
+            string dataPath = string.Empty;
+            string gamestringPath = string.Empty;
+
+            bool dataDuplicate = false;
+            bool gameStringDuplicate = false;
+
+            // check .hdp.json file for duplicates
+            JsonDocument hdpJsonDocument = JsonDocument.Parse(File.ReadAllBytes(Path.Join(_heroesDataDirectoryPath, versionString, _hdpJsonFile)));
+            if (hdpJsonDocument.RootElement.TryGetProperty("duplicate", out JsonElement duplicateElement))
+            {
+                if (duplicateElement.TryGetProperty("data", out JsonElement dataElement) && HeroesDataVersion.TryParse(dataElement.GetString(), out HeroesDataVersion? dataVersion))
+                {
+                    dataPath = Path.Join(
+                        _heroesDataDirectoryPath,
+                        dataVersion.ToString(),
+                        DataDirectoryName,
+                        string.Format(CultureInfo.InvariantCulture, _heroFileTemplateName, dataVersion.Build));
+
+                    dataDuplicate = true;
+                }
+
+                if (duplicateElement.TryGetProperty("gamestrings", out JsonElement gameStringsElement) && HeroesDataVersion.TryParse(gameStringsElement.GetString(), out HeroesDataVersion? gameStringsVersion))
+                {
+                    gamestringPath = Path.Join(
+                        _heroesDataDirectoryPath,
+                        gameStringsVersion.ToString(),
+                        GameStringsDirectoryName,
+                        string.Format(CultureInfo.InvariantCulture, _gameStringTemplateName, gameStringsVersion.Build, localization.ToString().ToLowerInvariant()));
+
+                    gameStringDuplicate = true;
+                }
+            }
+
+            if (!dataDuplicate)
+            {
+                dataPath = Path.Join(
+                    _heroesDataDirectoryPath,
+                    versionString,
+                    DataDirectoryName,
+                    string.Format(CultureInfo.InvariantCulture, _heroFileTemplateName, version.Build));
+            }
+
+            if (includeGameStrings)
+            {
+                if (!gameStringDuplicate)
+                {
+                    gamestringPath = Path.Join(
+                        _heroesDataDirectoryPath,
+                        versionString,
+                        GameStringsDirectoryName,
+                        string.Format(CultureInfo.InvariantCulture, _gameStringTemplateName, version.Build, localization.ToString().ToLowerInvariant()));
+                }
+
+                return HeroDataDocument.Parse(dataPath, GameStringDocument.Parse(gamestringPath, localization));
+            }
+
+            return HeroDataDocument.Parse(dataPath, localization);
+        }
+
+        private void VersionCheck(ref HeroesDataVersion version)
+        {
+            if (OldestVersion is null || NewestVersion is null)
+                return;
+
+            if (version < OldestVersion)
+                version = OldestVersion;
+            else if (version > NewestVersion)
+                version = NewestVersion;
         }
     }
 }
